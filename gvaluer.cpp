07d52e93615bb4574c7d221d68c4793f20bcbd80
Daniel Khaybulin <danielkhaybulin@gmail.com>
2021-09-28 17:40:44 +0300

    Change codestyle

--- gvaluer.cpp
+++ gvaluer.cpp
@@ -25,8 +25,7 @@
 
 using namespace std;
 
-enum
-{
+enum {
   RUN_OK               = 0,
   RUN_COMPILE_ERR      = 1,
   RUN_RUN_TIME_ERR     = 2,
@@ -53,9 +52,9 @@ enum
 static void
 die(const char *, ...)
     __attribute__((noreturn, format(printf, 1, 2)));
+
 static void
-die(const char *format, ...)
-{
+die(const char *format, ...) {
     va_list args;
     char buf[1024];
 
@@ -73,8 +72,7 @@ static bool interactive_flag;
 static bool rejudge_flag;
 static int locale_id = 0;
 
-static int parse_status(const string &str)
-{
+static int parse_status(const std::string &str) {
     if (str.length() != 2) return -1;
     char c1 = toupper(str[0]);
     char c2 = toupper(str[1]);
@@ -118,11 +116,11 @@ static int parse_status(const string &str)
 class ConfigParser;
 class Group
 {
-    string group_id;
+    std::string group_id;
     int first = 0;
     int last = 0;
-    vector<string> requires;
-    vector<string> sets_marked_if_passed;
+    std::vector<std::string> requires;
+    std::vector<std::string> sets_marked_if_passed;
     bool offline = false;
     bool sets_marked = false;
     bool skip = false;
@@ -134,76 +132,84 @@ class Group
     int test_score = -1;
     int pass_if_count = -1;
     int user_status = -1;
-
     int passed_count = 0;
     int total_score = 0;
-    string comment;
-
-    vector<set<int> > zero_sets;
-    set<int> passed_set;
+    std::string comment;
+    std::vector<std::set<int>> zero_sets;
+    std::set<int> passed_set;
 
 public:
     Group() {}
 
-    void set_group_id(const string &group_id_) { group_id = group_id_; }
-    const string &get_group_id() const { return group_id; }
+    void set_group_id(const std::string &group_id_) { group_id = group_id_; }
+    
+    const std::string &get_group_id() const { return group_id; }
 
-    void set_range(int first, int last)
-    {
+    void set_range(int first, int last) {
         this->first = first; 
         this->last = last;
     }
+
     int get_first() const { return first; }
+    
     int get_last() const { return last; }
 
-    void add_requires(const string &s) { requires.push_back(s); }
-    const vector<string> &get_requires() const { return requires; }
+    void add_requires(const std::string &s) { requires.push_back(s); }
+    
+    const std::vector<std::string> &get_requires() const { return requires; }
 
-    void add_sets_marked_if_passed(const string &s) { sets_marked_if_passed.push_back(s); }
-    const vector<string> &get_sets_marked_if_passed() const { return sets_marked_if_passed; }
+    void add_sets_marked_if_passed(const std::string &s) { sets_marked_if_passed.push_back(s); }
+    
+    const std::vector<std::string> &get_sets_marked_if_passed() const { return sets_marked_if_passed; }
 
     void set_offline(bool offline) { this->offline = offline; }
+    
     bool get_offline() const { return offline; }
 
     void set_sets_marked(bool sets_marked) { this->sets_marked = sets_marked; }
+    
     bool get_sets_marked() const { return sets_marked; }
 
     void set_skip(bool skip) { this->skip = skip; }
+    
     bool get_skip() const { return skip; }
 
     void set_skip_if_not_rejudge(bool skip) { this->skip_if_not_rejudge = skip; }
+    
     bool get_skip_if_not_rejudge() const { return skip_if_not_rejudge; }
 
     void set_stat_to_judges(bool stat) { this->stat_to_judges = stat; }
+    
     bool get_stat_to_judges() const { return stat_to_judges; }
 
     void set_stat_to_users(bool stat) { this->stat_to_users = stat; }
+    
     bool get_stat_to_users() const { return stat_to_users; }
 
     void set_score(int score) { this->score = score; }
+    
     int get_score() const { return score; }
 
     void set_pass_if_count(int count) { this->pass_if_count = count; }
+    
     int get_pass_if_count() const { return pass_if_count; }
 
     void set_test_all(bool value) { test_all = value; }
+    
     bool get_test_all() const { return test_all; }
 
     void inc_passed_count() { ++passed_count; }
+    
     int get_passed_count() const { return passed_count; }
-    bool is_passed() const
-    {
+    
+    bool is_passed() const {
         if (pass_if_count > 0) return passed_count >= pass_if_count;
         return passed_count == (last - first + 1);
     }
 
-    void add_passed_test(int test_num)
-    {
-        passed_set.insert(test_num);
-    }
+    void add_passed_test(int test_num) { passed_set.insert(test_num); }
 
-    bool is_zero_set() const
-    {
+    bool is_zero_set() const {
         for (int i = 0; i < int(zero_sets.size()); ++i) {
             if (passed_set == zero_sets[i])
                 return true;
@@ -211,35 +217,31 @@ public:
         return false;
     }
 
-    void set_comment(const string &comment_) { comment = comment_; }
-    const string &get_comment() const { return comment; }
+    void set_comment(const std::string &comment_) { comment = comment_; }
+    
+    const std::string &get_comment() const { return comment; }
+    
     bool has_comment() const { return comment.length() > 0; }
 
     void set_test_score(int ts) { test_score = ts; }
+    
     int get_test_score() const { return test_score; }
 
     void set_user_status(int user_status) { this->user_status = user_status; }
+    
     int get_user_status() const { return user_status; }
 
-    void add_zero_set(set<int> &&zs)
-    {
-        zero_sets.emplace_back(zs);
-    }
+    void add_zero_set(std::set<int> &&zs) { zero_sets.emplace_back(zs); }
 
     bool meet_requirements(const ConfigParser &cfg, const Group *& grp) const;
 
-    void add_total_score()
-    {
-        if (test_score > 0) total_score += test_score;
-    }
-    void set_total_score(int total_score)
-    {
-        this->total_score = total_score;
-    }
+    void add_total_score() { if (test_score > 0) total_score += test_score; }
+
+    void set_total_score(int total_score) { this->total_score = total_score; }
+
     int get_total_score() const { return total_score; }
 
-    int calc_score() const
-    {
+    int calc_score() const {
         if (test_score < 0 && passed_count == (last - first + 1)) {
             return score;
         } else if (test_score >= 0) {
@@ -249,25 +251,20 @@ public:
     }
 };
 
-class Global
-{
+class Global {
     int stat_to_judges = -1;
     int stat_to_users = -1;
 
 public:
-    void set_stat_to_judges(int value)
-    {
-        if (value < 0) {
-            value = -1;
-        } else if (value > 0) {
-            value = 1;
-        }
+    void set_stat_to_judges(int value) {
+        if (value < 0) value = -1;
+        else if (value > 0) value = 1;
         stat_to_judges = value;
     }
+
     int get_stat_to_judges() const { return stat_to_judges; }
 
-    void set_stat_to_users(int value)
-    {
+    void set_stat_to_users(int value) {
         if (value < 0) {
             value = -1;
         } else if (value > 0) {
@@ -275,18 +272,18 @@ public:
         }
         stat_to_users = value;
     }
+
     int get_stat_to_users() const { return stat_to_users; }
 };
 
-class ConfigParser
-{
+class ConfigParser {
 public:
     const int T_EOF = 256;
     const int T_IDENT = 257;
 
 private:
     FILE *in_f = NULL;
-    string path;
+    std::string path;
     int line;
     int pos;
 
@@ -294,27 +291,23 @@ private:
     int c_line;
     int c_pos;
 
-    string token;
+    std::string token;
     int t_type;
     int t_line;
     int t_pos;
 
     Global global;
-    vector<Group> groups;
+    std::vector<Group> groups;
 
 public:
-    ConfigParser()
-    {
-    }
+    ConfigParser() {}
 
-    ~ConfigParser()
-    {
+    ~ConfigParser() {
         if (!in_f) fclose(in_f);
         in_f = NULL;
     }
 
-    void next_char()
-    {
+    void next_char() {
         c_line = line;
         c_pos = pos;
         in_c = fgetc(in_f);
@@ -328,19 +321,21 @@ public:
         }
     }
 
-    void next_token()
-    {
+    void next_token() {
+        
         while (1) {
             while (isspace(in_c)) next_char();
             if (in_c != '#') break;
             while (in_c != EOF && in_c != '\n') next_char();
             if (in_c == '\n') next_char();
         }
+
         if (in_c == EOF) {
             t_type = T_EOF;
             token = "";
             return;
         }
+        
         if (isalnum(in_c) || in_c == '_') {
             token = "";
             t_type = T_IDENT;
@@ -352,10 +347,7 @@ public:
             }
             return;
         }
-        /*
-        if (in_c == '\"') {
-        }
-        */
+
         if (in_c == ';' || in_c == '{' || in_c == '}' || in_c == '-' || in_c == ',') {
             t_line = c_line;
             t_pos = c_pos;
@@ -364,14 +356,14 @@ public:
             next_char();
             return;
         }
+
         scan_error("invalid character");
     }
 
-    void scan_error(const string &msg) const;
-    void parse_error(const string &msg) const;
+    void scan_error(const std::string &msg) const;
+    void parse_error(const std::string &msg) const;
 
-    int read_int_opt(int default_value)
-    {
+    int read_int_opt(int default_value) {
         int value = default_value;
         if (t_type == T_IDENT) {
             try {
@@ -384,20 +376,24 @@ public:
         return value;
     }
 
-    void parse_group()
-    {
+    void parse_group() {
         Group g;
         bool has_stat_to_judges = false;
         bool has_stat_to_users = false;
 
         if (token != "group") parse_error("'group' expected");
+        
         next_token();
+        
         if (t_type != T_IDENT) parse_error("IDENT expected");
-        if (find_group(token) != NULL)
-            parse_error(string("group ") + token + " already defined");
+        
+        if (find_group(token) != NULL) parse_error(string("group ") + token + " already defined");
+        
         g.set_group_id(token);
         next_token();
+        
         if (t_type != '{') parse_error("'{' expected");
+        
         next_token();
         while (1) {
             if (token == "tests") {
@@ -453,7 +449,7 @@ public:
                 if (t_type != ';') parse_error("';' expected");
                 next_token();
             } else if (token == "0_if") {
-                set<int> zs;
+                std::set<int> zs;
                 try {
                     next_token();
                     int tn = stoi(token);
@@ -571,24 +567,24 @@ public:
                 break;
             }
         }
+        
         if (t_type != '}') parse_error("'}' expected");
+        
         next_token();
-        if (!has_stat_to_judges && global.get_stat_to_judges() >= 0) {
-            g.set_stat_to_judges(bool(global.get_stat_to_judges()));
-        }
-        if (!has_stat_to_users && global.get_stat_to_users() >= 0) {
-            g.set_stat_to_users(bool(global.get_stat_to_users()));
-        }
+
+        if (!has_stat_to_judges && global.get_stat_to_judges() >= 0) g.set_stat_to_judges(bool(global.get_stat_to_judges()));
+        
+        if (!has_stat_to_users && global.get_stat_to_users() >= 0) g.set_stat_to_users(bool(global.get_stat_to_users()));
         groups.push_back(g);
     }
 
-    void parse_groups()
-    {
-        while (token == "group") {
-            parse_group();
-        }
+    void parse_groups() {
+        while (token == "group") parse_group();
+
         if (groups.size() <= 0) parse_error("no groups defined");
+        
         sort(groups.begin(), groups.end(), [](const Group &g1, const Group &g2) -> bool { return g1.get_first() < g2.get_first(); });
+        
         for (int i = 1; i < int(groups.size()); ++i) {
             if (groups[i].get_first() <= groups[i - 1].get_last()) {
                 parse_error(string("groups ") + groups[i - 1].get_group_id() + " and " + groups[i].get_group_id() + " overlap");
@@ -597,6 +593,7 @@ public:
                 parse_error(string("hole between groups ") + groups[i - 1].get_group_id() + " and " + groups[i].get_group_id());
             }
         }
+
         for (int i = 0; i < int(groups.size()); ++i) {
             const vector<string> &r = groups[i].get_requires();
             for (int j = 0; j < int(r.size()); ++j) {
@@ -610,6 +607,7 @@ public:
                 }
             }
         }
+
         for (int i = 0; i < int(groups.size()); ++i) {
             const vector<string> &r = groups[i].get_sets_marked_if_passed();
             for (int j = 0; j < int(r.size()); ++j) {
@@ -623,6 +621,7 @@ public:
                 }
             }
         }
+
         int i;
         for (i = 0; i < int(groups.size()); ++i) {
             if (groups[i].get_offline())
@@ -637,8 +636,7 @@ public:
         }
     }
 
-    void parse_opt_global()
-    {
+    void parse_opt_global() {
         if (token != "global") return;
         next_token();
         if (t_type != '{') parse_error("'{' expected");
@@ -666,33 +664,30 @@ public:
         next_token();
     }
 
-    void parse(const string &configpath)
-    {
+    void parse(const string &configpath) {
         path = configpath;
         line = 1;
         pos = 0;
         in_f = fopen(configpath.c_str(), "r");
+
         if (!in_f) die("cannot open config file '%s'", configpath.c_str());
+        
         next_char();
         next_token();
         parse_opt_global();
         parse_groups();
-        if (token != "") {
-            parse_error("EOF expected");
-        }
+        
+        if (token != "") parse_error("EOF expected");
     }
 
-    const Group *find_group(const string &id) const
-    {
+    const Group *find_group(const string &id) const {
         for (auto i = groups.begin(); i != groups.end(); ++i) {
-            if (i->get_group_id() == id)
-                return &(*i);
+            if (i->get_group_id() == id) return &(*i);
         }
         return NULL;
     }
 
-    Group *find_group(int test_num)
-    {
+    Group *find_group(int test_num) {
         for (auto i = groups.begin(); i != groups.end(); ++i) {
             if (i->get_first() <= test_num && test_num <= i->get_last())
                 return &(*i);
@@ -703,27 +698,22 @@ public:
     const vector<Group> &get_groups() const { return groups; }
 };
 
-void
-ConfigParser::parse_error(const string &msg) const
-{
+void ConfigParser::parse_error(const string &msg) const {
     fprintf(stderr, "%s: %d: %d: parse error: %s\n", path.c_str(), t_line, t_pos, msg.c_str());
     exit(RUN_CHECK_FAILED);
 }
 
-void
-ConfigParser::scan_error(const string &msg) const
-{
+void ConfigParser::scan_error(const string &msg) const {
     fprintf(stderr, "%s: %d: %d: scan error: %s\n", path.c_str(), c_line, c_pos, msg.c_str());
     exit(RUN_CHECK_FAILED);
 }
 
-bool
-Group::meet_requirements(const ConfigParser &cfg, const Group *&grp) const
-{
+bool Group::meet_requirements(const ConfigParser &cfg, const Group *&grp) const {
     if (requires.size() <= 0) {
         grp = NULL;
         return true;
     }
+
     int i;
     const Group *gg = NULL;
     for (i = 0; i < int(requires.size()); ++i) {
@@ -731,17 +721,18 @@ Group::meet_requirements(const ConfigParser &cfg, const Group *&grp) const
         if (gg == NULL) die("group %s not found", requires[i].c_str());
         if (!gg->is_passed()) break;
     }
+
     if (i >= int(requires.size())) {
         grp = NULL;
         return true;
     }
+
     grp = gg;
     return false;
 }
 
-int
-main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]) {
     if (argc < 3 || argc > 4) die("invalid number of arguments");
 
     string self(argv[0]);
@@ -927,7 +918,6 @@ main(int argc, char *argv[])
                 fprintf(fcmt, "Test group '%s': tests %d-%d: score %d\n",
                         g.get_group_id().c_str(), g.get_first(), g.get_last(), group_score);
             }
-
         }
         if (g.get_offline()) {
             score += group_score;
